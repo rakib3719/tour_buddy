3fb7fc8da578bbfca60f1265b0b32a30b492ed98
rakib3719 <bannah76769@gmail.com>
2025-02-24 16:05:05 +0600

    payment page done

--- src/components/payment/Payment.tsx
+++ src/components/payment/Payment.tsx
@@ -1,23 +1,29 @@
-
-
+import paymentImage from '@/asset/image/payment 1.png'
+import Image from 'next/image';
+import sslImage from '@/asset/image/ssl2 1.png'
 const Payment = () => {
     return (
-        <div>
-            <h1 className="text-2xl font-bold">
+        <div className=''>
+            <h1 className="text-2xl px-4 font-bold">
                 Payment
                 </h1>
 
-                <div className='bg-[#f9fafd] py-8'>
-                <section className="container  mx-auto">
+                <div className='bg-[#f9fafd] px-4 py-8'>
+                <section className="container   mx-auto">
 
-<div className=" mt-8">
+<div className=" mt-8 ">
 <h1 className="text-2xl">Choose Payment Method</h1>
 {/* payment form */}
 
 
 <form  className="mt-4">
 
-<div className="w-full ">
+<div className='flex flex-col md:flex-row justify-between gap-24'>
+
+
+    <div className='md:w-[80%]'>
+
+    <div className="w-full ">
 <label
 htmlFor="name"
 className="text-lg font-semibold"
@@ -84,6 +90,74 @@ className="placeholder:bg-[#f0f0f1] bg-[#f0f0f1] outline-none py-3 mt-1 px-4 w-f
 />
 </div>
 
+</div>
+
+{/* image */}
+
+{/*payment image */}
+<div className=' '>
+<Image src={paymentImage} alt='payment image'/>
+</div>
+</div>
+
+
+
+
+{/* select method */}
+<div className='mt-12'>
+  <h1 className='font-semibold my-2'>Select method:</h1>
+  <div className='flex flex-wrap gap-4'>
+    
+    <label htmlFor="bkash">
+      <div className='bg-[#37B1E2] flex items-center rounded-full p-4 gap-2 w-32'>
+        <input type="radio" id="bkash" name="paymentMethod" /> 
+        <h4 className='text-white font-semibold'>Bkash</h4>
+      </div>
+    </label>
+
+    <label htmlFor="nagad">
+      <div className='bg-[#37B1E2] flex items-center rounded-full p-4 gap-2 w-32'>
+        <input type="radio" id="nagad" name="paymentMethod" /> 
+        <h4 className='text-white font-semibold'>Nagad</h4>
+      </div>
+    </label>
+
+    <label htmlFor="upay">
+      <div className='bg-[#37B1E2] flex items-center rounded-full p-4 gap-2 w-32'>
+        <input type="radio" id="upay" name="paymentMethod" /> 
+        <h4 className='text-white font-semibold'>Upay</h4>
+      </div>
+    </label>
+
+    <label htmlFor="visa">
+      <div className='bg-[#37B1E2] flex items-center rounded-full p-4 gap-2 w-32'>
+        <input type="radio" id="visa" name="paymentMethod" /> 
+        <h4 className='text-white font-semibold'>Visa</h4>
+      </div>
+    </label>
+
+    <label htmlFor="mastercard">
+      <div className='bg-[#37B1E2] flex items-center rounded-full p-4 gap-2 w-32'>
+        <input type="radio" id="mastercard" name="paymentMethod" /> 
+        <h4 className='text-white font-semibold'>MasterCard</h4>
+      </div>
+    </label>
+
+    <label htmlFor="others">
+      <div className='bg-[#37B1E2] flex items-center rounded-full p-4 gap-2 w-32'>
+        <input type="radio" id="others" name="paymentMethod" /> 
+        <h4 className='text-white font-semibold'>Others</h4>
+      </div>
+    </label>
+
+  </div>
+</div>
+
+
+
+<button type='submit' className='bg-[#13253F] rounded-full py-4 px-12 text-white mx-auto mt-8 flex justify-center'>
+Confirm Payment
+</button>
 </form>
 
 
@@ -91,9 +165,12 @@ className="placeholder:bg-[#f0f0f1] bg-[#f0f0f1] outline-none py-3 mt-1 px-4 w-f
 
 </div>
 
-<div>
-    
-</div>
+
+</section>
+
+
+<section className='mx-auto  flex justify-center mt-8'>
+    <Image src={sslImage} alt='ssl Image'/>
 </section>
                 </div>
                
